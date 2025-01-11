6af41a65ee8fccd346292b3a2e6b73a071869311
june <0721hjp@naver.com>
2025-01-11 14:56:27 +0900

    Fix: table이름 지정

--- src/main/java/com/example/bancartontrendage/Entity/MemeEntity.java
+++ src/main/java/com/example/bancartontrendage/Entity/MemeEntity.java
@@ -1,16 +1,14 @@
 package com.example.bancartontrendage.Entity;
 
 import com.example.bancartontrendage.Dto.MemeDto;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "meme")
 public class MemeEntity {
     @Id
     @GeneratedValue
--- src/main/java/com/example/bancartontrendage/Entity/UserEntity.java
+++ src/main/java/com/example/bancartontrendage/Entity/UserEntity.java
@@ -1,16 +1,14 @@
 package com.example.bancartontrendage.Entity;
 
 import com.example.bancartontrendage.Dto.UserDto;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "user")
 public class UserEntity {
     @Id
     @GeneratedValue
