5539674a27630b3da8fd1cba4362cf7e1cab7de4
moonjun1 <puls55555@naver.com>
2025-01-11 20:19:44 +0900

    Fix: create 추가

--- src/main/java/com/example/bancartontrendage/RestController/UserController.java
+++ src/main/java/com/example/bancartontrendage/RestController/UserController.java
@@ -1,4 +1,6 @@
 package com.example.bancartontrendage.RestController;
+
+// 필요한 클래스들을 import합니다.
 import com.example.bancartontrendage.Dto.UserDto;
 import com.example.bancartontrendage.Entity.UserEntity;
 import com.example.bancartontrendage.Service.UserService;
@@ -7,22 +9,30 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/users")
 public class UserController {
 
+    // UserService를 자동으로 주입받습니다.
     @Autowired
     private UserService userService;
 
+    // "/create" 엔드포인트에 대한 GET 요청을 처리합니다.
     @GetMapping("/create")
     public UserDto createUser(@RequestParam String nickname) {
+        // UserDto 객체를 생성하고 닉네임을 설정합니다.
         UserDto userDto = new UserDto();
         userDto.setNickname(nickname);
+
+        // UserService를 사용하여 UserEntity 객체를 생성합니다.
         UserEntity userEntity = userService.createUser(userDto);
-        UserDto responseDto = new UserDto();
-        responseDto.setId(userEntity.getId());
-        responseDto.setNickname(userEntity.getNickname());
-        responseDto.setAge(userEntity.getAge());
-        responseDto.setLevel(userEntity.getLevel());
-        return responseDto;
+
+        // 응답으로 반환할 UserDto 객체를 생성하고, UserEntity의 데이터를 설정합니다.
+        
+        userDto.setId(userEntity.getId());
+        userDto.setNickname(userEntity.getNickname());
+        userDto.setAge(userEntity.getAge());
+        userDto.setLevel(userEntity.getLevel());
+
+        // UserDto 객체를 반환합니다.
+        return userDto;
     }
 }
